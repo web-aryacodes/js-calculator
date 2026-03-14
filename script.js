7f03e210d80ba1b3d76fa91046ed0a760ca34051
Arya Patil <work.aryapatil3455@gmail.com>
2026-03-14 16:35:27 +0530

    Added keyboard support to the calculator

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 let display = document.getElementById("display");
 
 function append(value){
-display.value += value;
+    display.value += value;
 }
 
 function clearDisplay(){
-display.value = "";
+    display.value = "";
 }
 
 function deleteLast(){
-display.value = display.value.slice(0,-1);
+    display.value = display.value.slice(0,-1);
 }
 
 function calculate(){
-try{
-display.value = eval(display.value);
+    display.value = eval(display.value);
 }
-catch{
-display.value = "Error";
-}
-}
+
+/* Adding Keyboard Support */
+
+document.addEventListener("keydown", function(event){
+
+    let key = event.key;
+
+    if(!isNaN(key) || "+-*/.".includes(key)){
+        append(key);
+    }
+
+    if(key === "Enter"){
+        calculate();
+    }
+
+    if(key === "Backspace"){
+        deleteLast();
+    }
+
+    if(key === "Escape"){
+        clearDisplay();
+    }
+
+});
