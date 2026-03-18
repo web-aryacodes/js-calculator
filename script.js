91a021b04a5d33696060bd543e8e12046693a57a
Arya Patil <work.aryapatil3455@gmail.com>
2026-03-18 20:50:50 +0530

    improved history sidebar, clear history feature, and  calculator UI layout

--- script.js
+++ script.js
@@ -25,31 +25,46 @@ function append(value){
     }
 
     display.value += value;
+    adjustFontSize();
 }
+
 function clearDisplay(){
     display.value = "";
+    adjustFontSize();
 }
 
 function deleteLast(){
     display.value = display.value.slice(0,-1);
+    adjustFontSize();
 }
 
 function calculate(){
 
     let expression = display.value;
-    let result = eval(expression);
-    display.value = result;
 
-    let history = document.getElementById("history");
+    // remove ending operator
+    while("+-*/".includes(expression.slice(-1))){
+        expression = expression.slice(0,-1);
+    }
 
-    let entry = document.createElement("p");
-    entry.textContent = expression + " = " + result;
+    try{
+        let result = eval(expression);
+        display.value = result;
 
-    history.appendChild(entry);
+        let history = document.getElementById("history");
 
+        let entry = document.createElement("p");
+        entry.textContent = expression + " = " + result;
+
+        history.appendChild(entry);
+    }catch{
+        display.value = "Error";
+    }
+
+    adjustFontSize();
 }
 
-/* Adding Keyboard Support */
+/* Keyboard Support */
 
 document.addEventListener("keydown", function(event){
 
@@ -72,18 +87,42 @@ document.addEventListener("keydown", function(event){
     }
 
 });
+
 function clearHistory(){
     document.getElementById("history").innerHTML = "";
 }
 
 function percentage(){
     display.value = display.value / 100;
+    adjustFontSize();
 }
 
 function square(){
     display.value = Math.pow(display.value, 2);
+    adjustFontSize();
 }
 
 function squareRoot(){
     display.value = Math.sqrt(display.value);
+    adjustFontSize();
+}
+
+/* Auto Font Resize */
+
+function adjustFontSize(){
+
+    let length = display.value.length;
+
+    if(length < 10){
+        display.style.fontSize = "28px";
+    }
+    else if(length < 20){
+        display.style.fontSize = "22px";
+    }
+    else if(length < 30){
+        display.style.fontSize = "18px";
+    }
+    else{
+        display.style.fontSize = "14px";
+    }
 }
